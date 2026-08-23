9efbba23c6bafaf8f3e3e54b861067e4cfc46f7a
Vladimir Voitenko <vladimirdev635@gmail.com>
2026-08-23 16:22:48 +0500

    remove-aws-lc-sys-dependency

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 mod cli;
 
 fn main() {
+    rustls::crypto::ring::default_provider()
+        .install_default()
+        .expect("Failed to install rustls ring crypto provider");
     usage::Run::run(cli::App::parse());
 }
